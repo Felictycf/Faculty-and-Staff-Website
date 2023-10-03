a07528abfb40f4136c36722da6b69d88473f65cf
Forward <felicitycfgreene@gmail.com>
2023-10-04 02:41:31 +0800

    美化完template3的内容

--- front-end/src/views/profile/basic/template3.vue
+++ front-end/src/views/profile/basic/template3.vue
@@ -7,7 +7,7 @@
         <img :width="200" :src="userInfo.avatar" />
 
         <h1>
-          <b style="font-size: 30px">{{ userInfo.firstName }} {{ userInfo.lastName }}</b>
+          <b style="font-size: 30px;color: #f5f5f5">{{ userInfo.firstName }} {{ userInfo.lastName }}</b>
         </h1>
 
         <div class="Departments">
@@ -26,28 +26,25 @@
         <!-- <a-row style="margin-top: 10px">
           <h2 class="title">{{ userInfo.title }}</h2>
         </a-row> -->
-
         <a-row style="margin-top: 30px; font-weight: 600">
-          <a-row>{{ userInfo.phone }}</a-row>
-          <a-row>{{ userInfo.email }}</a-row>
+          <a-row><a-icon type="phone" /> {{ userInfo.phone }}</a-row>
+          <a-row><a-icon type="mail" /> {{ userInfo.email }}</a-row>
         </a-row>
 
         <a-row>
-          <p>Teaching</p>
+          <p><a-icon type="book" /> Teaching : </p>
           <ul>
-            <li v-for="(item, index) in userInfo.teaching" :key="item.time" style="list">
-              {{ index + 1 }}. {{ item.description }}
+            <li v-for="(item, index) in userInfo.teaching" :key="item.time"  >
+              {{ index + 1 }}. {{ item.description }} ({{item.time}})
             </li>
           </ul>
         </a-row>
 
-        <a-row>
-          <a-col :span="12">Degtees: {{ userInfo.degtees }}</a-col>
-        </a-row>
+
       </a-col>
       <a-col :span="8" style="background-color: white; padding: 20px">
         <div class="list">
-          <h2>Degrees</h2>
+          <h2 style=" color: #000;"><a-icon type="graduation-cap" /> Degrees</h2>
           <a-row v-for="(item, index) in userInfo.degrees" :key="item.time">
             <a-row>
               <b>{{ index + 1 }}</b>
@@ -55,7 +52,7 @@
               {{ item.time }}
             </a-row>
           </a-row>
-          <h2 style="margin-top: 100px">Academic Position</h2>
+          <h2 style="margin-top: 30px ;color: #000;"><a-icon type="idcard" /> Academic Position</h2>
           <a-row v-for="(item, index) in userInfo.aacdimicPosition" :key="item.time">
             <a-row>
               <b>{{ index + 1 }}</b>
@@ -63,25 +60,26 @@
               {{ item.time }}
             </a-row>
           </a-row>
-          <h2 style="margin-top: 100px">Publications</h2>
+          <h2 style="margin-top: 30px;color: #000;"><a-icon type="book-open" /> Publications</h2>
           <a-row v-for="(item, index) in userInfo.publications" :key="item.time">
             <b>{{ index + 1 }}</b>
 
 <!--            <img :src="item.img" alt="" width="100px" />-->
+            {{ item.title }}
             {{ item.time }}
-            {{ item.description }}
+            <div class="limited-description">{{ item.description }}</div>
             <a :href="item.pdf" :download="item.pdf">pdf</a>
-            {{ item.description }}
+
           </a-row>
         </div>
       </a-col>
 
       <a-col :span="8" style="padding: 0 20px">
-        <h2>Recent Highlights</h2>
+        <h2 >Recent Highlights</h2>
         <div class="list">
           <div class="item" style="margin-bottom: 30px" v-for="item in userInfo.highlights" :key="item.time">
             <h2>{{ item.time }}</h2>
-            <b>{{ item.description }}</b>
+            <b style="color: #f5f5f5" >{{ item.description }}</b>
           </div>
         </div>
 
@@ -120,6 +118,30 @@ export default {
 </script>
 
 <style lang="less" scoped>
+/* ... 你现有的样式 ... */
+.limited-description {
+  .ell-3;
+
+  // 可能需要一个固定的宽度或最大宽度来确保ellipsis正常工作
+  max-width: 80%;  // 或根据你的需要调整
+}
+
+/* 为图标添加基本的样式和间距 */
+a-icon {
+  margin-right: 8px;
+}
+
+/* 使用ellipsis来限制显示的描述长度 */
+.ell-3 {
+  overflow: hidden;
+  display: -webkit-box;
+  -webkit-box-orient: vertical;
+  -webkit-line-clamp: 5;
+}
+
+/* 在需要的地方应用这个样式，例如 */
+
+
 .ell-1 {
   overflow: hidden;
   display: -webkit-box;
@@ -144,6 +166,7 @@ body {
   background-color: #464d63;  // 更亮的背景
   padding: 30px;
   border-radius: 10px;
+  color: #f5f5f5;
   box-shadow: 0 4px 10px rgba(0, 0, 0, 0.1);
 }
 
@@ -158,7 +181,7 @@ h1 {
 
 h2 {
   font-size: 24px;
-  color: #333;
+  color: #f5f5f5;
   border-bottom: 2px solid #608ba3b8;
   padding-bottom: 5px;
 }
@@ -188,8 +211,10 @@ a-row {
 
 /* Teaching, Degrees, and Academic Position sections */
 .list {
-  background-color: #ffffff;
-  padding: 20px;
+
+  //background-color: #ffffff;
+  color: #000;
+  padding: 10px;
   border-radius: 8px;
   box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
 }
@@ -223,6 +248,8 @@ a-row {
 
 .publication-description {
   flex: 1;
+  // 将其应用到出版物描述上
+  .ell-3;
 }
 
 .pdf-link {
